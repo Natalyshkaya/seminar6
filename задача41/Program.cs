a78a7d9bf6af9eab1a8d55d48c480594684a168a
Nataly <naty.sh@mail.ru>
2022-12-05 22:20:14 +0500

    задача 41

---
+++ задача41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] CreateMassive(int m)
+{
+    int[] array = new int[m];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i+1} число");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+    return array;
+}
+
+int CoantPositive(int[] array)
+{
+    int coant = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            coant++;
+        }
+    }
+    return coant;
+}
+
+Console.WriteLine("Введите количество элементов массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateMassive(m);
+Console.WriteLine();
+Console.WriteLine($"количество положительных чисел={CoantPositive(array)}");
